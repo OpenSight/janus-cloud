25bc353b1b8e2a0ad14755f2add97e95e0d138d1
jiankai <wangjiankai@cmhi.chinamobile.com>
2020-03-29 13:37:14 +0800

    bug fix

--- januscloud/proxy/rest/__init__.py
+++ januscloud/proxy/rest/__init__.py
@@ -4,6 +4,5 @@
 def includeme(config):
     # look into following modules' includeme function
     # in order to register routes
-    config.include(__name__ + '.client')
     config.include(__name__ + '.backend_server_view')
     config.scan()
