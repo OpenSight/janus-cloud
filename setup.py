69780c6e143a7f67f178e448f97ee83606ce1bd3
jiankai <wangjiankai@cmhi.chinamobile.com>
2020-06-21 22:18:19 +0800

    depend the certain version of other package

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ requires = [
     'ws4py==0.5.1',
     'PyYAML==5.1',
     'pyramid==1.10.4',
-    'requests>=2.18.4',
+    'requests==2.18.4',
     'python-daemon==2.2.3',
-    'redis>=3.5.2'
+    'redis==3.5.2'
 ]
 
 setup(name='janus-cloud',
