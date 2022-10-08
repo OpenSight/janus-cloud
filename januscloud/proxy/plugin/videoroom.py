97db56421705caf215ae78d4e122bd6e8530a5f6
jiankai <wangjiankai@cmhi.chinamobile.com>
2022-10-08 15:46:49 +0800

    fix issue #40

--- januscloud/proxy/plugin/videoroom.py
+++ januscloud/proxy/plugin/videoroom.py
@@ -2311,6 +2311,7 @@ class BackendRoom(object):
             'permanent': False,
             'is_private': False,
             'require_pvtid': False,
+            'publishers': 10000,       # no publisher number limitation
             'bitrate': room.bitrate,
             'fir_freq': room.fir_freq,
             'audiocodec': ','.join(room.audiocodec),
